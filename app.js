f1864a251a291cd17b383675db593d570301dbed
itsmeroxanna <111817059+itsmeroxanna@users.noreply.github.com>
2023-09-17 19:38:20 -0500

    edit
    update.

---
+++ app.js
@@ -0,0 +1,21 @@
+const name="Hello Web Programming";
+
+console.log("Lets do this");
+
+function greet() {
+    alert("Hello World!");
+}
+
+function greet(firstName, lastName) {
+    alert("Hello " + firstName + " " + lastName);
+}
+
+greet("Dr.", "Susan Escobar");
+
+const person = {
+    firstName: "Roxanna",
+    lastName : "Torres",
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
